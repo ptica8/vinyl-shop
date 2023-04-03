59a4e7f7855dd1b3e901f3d46693a5df497535b4
Valeria Mikshina <lera-mikshina@mail.ru>
2023-04-04 00:03:13 +0200

    add Header for mobile version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,14 @@ export default function Header() {
 	return (
 		<header className="header">
 			<div className="header__container header__container-1">
+				<button className="header__burger"/>
 				<Link className="header__link-main" to="/">SPINLATE</Link>
-				<Link className="header__links" to="/">Popular</Link>
-				<Link className="header__links" to="/">New Items</Link>
-				<Link className="header__links" to="/">Sale</Link>
+				<Link className="header__links header__links_left" to="/">Popular</Link>
+				<Link className="header__links header__links_left" to="/">New Items</Link>
+				<Link className="header__links header__links_left" to="/">Sale</Link>
 			</div>
-			<hr className="line header__line"/>
 			<div className="header__container header__container-2">
-				<Link className="header__links" to="/">Info</Link>
+				<Link className="header__links header__links_info" to="/">Info</Link>
 				<Link className="header__links" to="/">Cart (0)</Link>
 				<button type="submit" className="header__button header__links">
 					<div className="header__button_img"></div>
